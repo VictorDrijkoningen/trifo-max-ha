6fede51f1093c01bad2e1dda32b9eeebb03858df
VictorDrijkoningen <72612250+VictorDrijkoningen@users.noreply.github.com>
2025-04-08 16:21:34 +0200

    lets

--- trifomaxha.py
+++ trifomaxha.py
@@ -94,3 +94,23 @@ run()
 import helpers
 
 print(helpers.test())
+
+import asyncio
+from microdot import Microdot
+
+app = Microdot()
+
+@app.route('/')
+async def index(request):
+    return 'Hello, world!'
+
+async def main():
+    # start the server in a background task
+    server = asyncio.create_task(app.start_server())
+
+    # ... do other asynchronous work here ...
+
+    # cleanup before ending the application
+    await server
+
+asyncio.run(main())
