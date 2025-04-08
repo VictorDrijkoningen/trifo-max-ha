252802b7c722a48524c909c7ea439cb91cab7981
VictorDrijkoningen <72612250+VictorDrijkoningen@users.noreply.github.com>
2025-04-08 17:11:52 +0200

    no

--- builder.sh
+++ builder.sh
@@ -69,6 +69,6 @@ run_docker() {
 }
 
 case $ARCH in
-        "aarch64") run_docker "linux/arm64" "arm64v8/python:3.9.17-buster" ;;
+        "aarch64") run_docker "linux/arm64" "arm64v8/python:3.7.11-stretch" ;;
         *) help;;
 esac
