29eddc6f355676265fa36c820ff85aadc9c88303
VictorDrijkoningen <72612250+VictorDrijkoningen@users.noreply.github.com>
2025-04-12 21:42:47 +0200

    reput logging

--- trifomaxha.py
+++ trifomaxha.py
@@ -18,11 +18,10 @@ auth = BasicAuth(app)
 @auth.authenticate
 async def verify_user(request, username, password):
     if password == env['webserver_access_key']:
-        print(f"successful login from")
+        print(f"successful login from {str(request.client_addr)}")
         return True
     else:
         print(f"wrong login from {str(request.client_addr)}")
-        # {str(request.client_addr)}
 
 @app.route('/')
 async def index(request):
